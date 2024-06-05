badaca94709a9f3f82819dad5b686e2acc290397
DuckySmacky <kriperoka@gmail.com>
2024-06-05 20:14:37 +0300

    app init
    Android 8.1 Build

---
+++ app/app/src/main/java/com/github/duckysmacky/signsense/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.duckysmacky.signsense.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.github.duckysmacky.signsense.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
